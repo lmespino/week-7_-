e9c19a0be4c55afcbbe81306a35b2e875141416b
lmespino01~ <lmespino@cps.edu>
2024-12-03 15:13:23 -0600

    list comprehension fully complete

--- learning/list_comprehension.py
+++ learning/list_comprehension.py
@@ -32,10 +32,10 @@
 # print(odd_nums)
 
 
-grades = [85, 42, 79, 90, 56, 61, 30]
-passing_grades = [grade for grade in grades if grade >= 60]
+# grades = [85, 42, 79, 90, 56, 61, 30]
+# passing_grades = [grade for grade in grades if grade >= 60]
 
-print(passing_grades)
+# print(passing_grades)
 
 
 ################################################List comprehension###############################################
@@ -44,20 +44,22 @@ print(passing_grades)
 
 # Create a square_values list consisting of the numbers in the values list, squared.
 
-# values = [1, 2, 3, 4, 5, 6, 9.5]
+values = [1, 2, 3, 4, 5, 6, 9.5]
 
+squared_values = [value * value for value in values]
 
-
+print(squared_values)
 
 # List Comprehensions Practice #2
 # To do the coding exercise below, you can choose different paths. While the correct path in programming is the one that returns the correct result, I encourage you to try applying list comprehension concepts to begin to assimilate them for the future. They can be very useful in your professional practice!
 
 # Create an even_values list consisting of the numbers in the values list that (you guessed it!) are even.
 
-# values = [1, 2, 3, 4, 5, 6, 9.5]
+values = [1, 2, 3, 4, 5, 6, 9.5]
 
+even_values = [value for value in values if value % 2 == 0]
 
-
+print(even_values)
 
 # List Comprehensions Practice #3
 # To do the coding exercise below, you can choose different paths. While the correct path in programming is the one that returns the correct result, I encourage you to try applying list comprehension concepts to begin to assimilate them for the future. They can be very useful in your professional practice!
